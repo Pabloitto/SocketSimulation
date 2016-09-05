7cfa2619729010669586b4063f037a8e899d9b08
pabloitto <juanitto_40@hotmail.com>
2016-09-05 14:33:04 -0700

    x

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var net = require("net");
 var server = net.createServer(function (socket) {
-    socket.write('Echo server\r\n');
+    socket.write('90');
 	socket.pipe(socket);
     /*socket.setEncoding("utf8");
     socket.on('data', function (data) {
