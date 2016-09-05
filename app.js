9e9a434044fdb0134af861741b20e8efaea84221
pabloitto <juanitto_40@hotmail.com>
2016-09-05 13:53:09 -0700

    fix

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var net = require("net");
-var tcp_server = net.createServer(function (socket) {
+var server = net.createServer(function (socket) {
     socket.setEncoding("utf8");
     socket.on('data', function (data) {
-        var delay = Math.floor((Math.random() * 20) + 1) * 1000;
-        console.log('Estimated time ' + delay);
-        setTimeout(function () {
-            try {
-                console.log('Complete');
-                socket.write("90");
-            } catch (error) {
-                socket.write("99");
-            }
-        }, delay);
+        try {
+            console.log('Complete');
+            socket.write("90");
+        } catch (error) {
+            socket.write("99");
+        }
+        socket.end();
     });
+    //socket.end("goodbye\n");
 });
-tcp_server.listen(process.env.port || 8000);
+
+// grab a random port.
+server.listen(process.env.port || 8000,function () {
+    address = server.address();
+    console.log("opened server on %j", address);
+});
